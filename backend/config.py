d307aee13467ae79e25e49dde7f5a746965dcef5
bhawna <bhawnavishwakarma464@gmail.com>
2026-06-29 12:30:32 +0530

    updated config.py

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://"
+        f"{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET")
+    print("DB URI:", SQLALCHEMY_DATABASE_URI)
